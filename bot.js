7b43e90aaeb9ba10a1b0c7f0b042d1b90e9aa8d9
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 14:31:23 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
+const { Telegraf } = require("telegraf");
+const { BOT_TOKEN } = require("./config");
 
+const start = require("./handlers/start");
+const signal = require("./handlers/signal");
+
+const bot = new Telegraf(BOT_TOKEN);
+
+start(bot);
+signal(bot);
+
+bot.launch();
+
+console.log("🤖 Trading Bot Running...");
