e26acaddba8c1831fb8101fa634ac415a6b324c4
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 15:44:43 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,27 @@
 require("dotenv").config();
 
-const { Telegraf } = require("telegraf");
+const { Telegraf, session } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Session (selected market, timeframe, last photo save করার জন্য)
+bot.use(session());
+
+// Handlers
 const start = require("./hanslers/start");
 const signal = require("./hanslers/signal");
+const callback = require("./hanslers/callback");
 
 // Load handlers
 start(bot);
 signal(bot);
+callback(bot);
 
-// Start bot
+// Bot start
 bot.launch();
 
 console.log("🤖 Trading Signal Bot Running...");
 
-// Stop gracefully
+// Graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
