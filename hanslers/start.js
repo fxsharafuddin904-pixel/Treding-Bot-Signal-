4d0cab3cb91abfb6291cb9a5566599f9f2429493
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 14:32:09 +0600

    Update start.js

--- hanslers/start.js
+++ hanslers/start.js
@@ -1,1 +1,10 @@
+const { menu } = require("../utils/keyboards");
 
+module.exports = (bot) => {
+  bot.start((ctx) => {
+    ctx.reply(
+      "🤖 AI Trading Signal Bot\n\nChoose an option:",
+      menu
+    );
+  });
+};
