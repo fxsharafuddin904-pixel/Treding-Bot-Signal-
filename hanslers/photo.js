9ac92ed79f64aca60b61c32ca7856254f55098f0
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 16:21:54 +0600

    Delete hanslers directory

--- hanslers/photo.js
+++
@@ -1,8 +0,0 @@
-bot.on("photo", async (ctx) => {
-  users[ctx.from.id] = {
-    ...users[ctx.from.id],
-    lastPhoto: ctx.message.photo.pop().file_id
-  };
-
-  ctx.reply("⏳ Analysing chart...");
-});
