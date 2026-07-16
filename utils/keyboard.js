47ecbe64b63d92cf0c1a224326f6f25e37c76797
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 14:31:47 +0600

    Update keyboard.js

--- utils/keyboard.js
+++ utils/keyboard.js
@@ -1,1 +1,20 @@
+const menu = {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        {
+          text: "📈 Get Signal",
+          callback_data: "signal"
+        }
+      ],
+      [
+        {
+          text: "🔄 Refresh Signal",
+          callback_data: "signal"
+        }
+      ]
+    ]
+  }
+};
 
+module.exports = { menu };
