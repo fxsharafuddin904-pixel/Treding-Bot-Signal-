dbcd373ea2d296978294b28807424498a3b9045c
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-16 14:32:30 +0600

    Update signalServices.js

--- services/signalServices.js
+++ services/signalServices.js
@@ -1,1 +1,12 @@
+const axios = require("axios");
+const { API_URL } = require("../config");
 
+module.exports = async () => {
+  try {
+    const res = await axios.get(API_URL);
+
+    return res.data;
+  } catch (err) {
+    return null;
+  }
+};
